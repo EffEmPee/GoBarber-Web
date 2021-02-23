d5dfed8b746bb0b66b1f758e8914032a1c83ea9e
Felipe Pierotti <felipemacacari@gmail.com>
2021-02-23 20:09:48 -0300

    month availability

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@Gobarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Beares ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@Gobarber:token', token);
     localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Beares ${token}`;
 
     setData({ token, user });
   }, []);
